035f3c646dfa31a9093e9bb9ad152a4fb2cdacad
Guilherme Araújo Diniz <al.araujo.diniz@impatech.edu.br>
2026-09-07 09:27:52 -0300

    Programa que imprime as constantes 'pi' e 'e' com o numero informado de casas decimasi

--- aproximacoes.py
+++ aproximacoes.py
@@ -1,7 +1,10 @@
-import MYTOOLS
+import MYTOOLS as mt
+
 casas_pi = int(input("Quantas casas decimais de PI?"))
 casas_e = int(input("Quantas casas decimais de E?"))
-aprox_pi = MYTOOLS.pi_real(casas_pi)
-aprox_e = MYTOOLS.e_real(casas_e)
+
+aprox_pi = mt.pi_real(casas_pi)
+aprox_e = mt.e_real(casas_e)
+
 print(f"""Pi = {aprox_pi}
 E = {aprox_e}""")
